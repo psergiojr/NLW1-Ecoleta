59c0097203d97442d90cb32e92c888b674f140a7
Paulo Sergio Jr <paulo.psw@gmail.com>
2020-06-03 00:14:07 -0300

    Reorganizando rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,43 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-const users = [
-    'Diego',
-    'Cleiton',
-    'Robson',
-    'Rafael'
-];
-
-app.get('/users', (request, response) => {
-    const search = String(request.query.search);
-
-    console.log(search);
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-    
-    response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-
-    const user = users[id];
-
-    return response.json(user);
-});
-
-app.post('/users', (request, response) => {
-    const data = request.body;
-    
-    const user = {
-        name: data.nome,
-        email: data.email
-    };
-    
-    return response.json(user);
-})
+app.use(routes);
 
 app.listen(3333)
