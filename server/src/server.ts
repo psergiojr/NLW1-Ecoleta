4b248870f74b77c12a6bf26f04d353332e475aa4
Paulo Sergio Jr <paulo.psw@gmail.com>
2020-06-02 23:05:22 -0300

    Rotas e parâmetros

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,42 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+const users = [
+    'Diego',
+    'Cleiton',
+    'Robson',
+    'Rafael'
+];
+
 app.get('/users', (request, response) => {
-    response.json([
-        'Diego',
-        'Cleiton',
-        'Robson',
-        'Rafael'
-    ]);
+    const search = String(request.query.search);
+
+    console.log(search);
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+    
+    response.json(filteredUsers);
 });
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+    
+    const user = {
+        name: data.nome,
+        email: data.email
+    };
+    
+    return response.json(user);
+})
 
 app.listen(3333)
