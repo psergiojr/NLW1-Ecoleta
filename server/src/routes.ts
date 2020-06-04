6b5f7a43c9bbaf87bbdeb3675c93c9fd843bc277
Paulo Sergio Jr <paulo.psw@gmail.com>
2020-06-04 12:03:56 -0300

    Add rotas GET items e POST points

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
@@ -7,5 +8,56 @@ routes.get('/', (request, response) => {
         "message": "hello rocket"
     })
 });
+
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return {
+            id: item.id,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        }
+    });
+
+    return response.json(serializedItems);
+});
+
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    const insertedIds = await knex('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    });
+
+    const point_id = insertedIds[0];
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id
+        };
+    })
+
+    await knex('points_items').insert(pointItems);
+
+    return response.json({ success: true });
+})
 
 export default routes;
