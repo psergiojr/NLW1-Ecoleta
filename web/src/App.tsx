363eab8233b609855e2573367703ec5c8bd88192
Paulo Sergio Jr <paulo.psw@gmail.com>
2020-06-04 22:48:18 -0300

    Add Pagina Home

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Home from './pages/Home';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick() {
-    setCounter(counter+1);
-  }
-
   return (
     <div>
-      <Header title="Buenas noches Macarena" />
-
-      <h1>{counter}</h1>
-      <h1>{counter * 2}</h1>
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+      <Home/>
     </div>
   );
 }
