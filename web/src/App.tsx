4a21f4c46685af71e27923136ef84d2838070366
Paulo Sergio Jr <paulo.psw@gmail.com>
2020-06-04 21:42:41 -0300

    Estado e imutabilidade

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    setCounter(counter+1);
+  }
+
   return (
-    <Header title="Buenas noches Macarena" />
+    <div>
+      <Header title="Buenas noches Macarena" />
+
+      <h1>{counter}</h1>
+      <h1>{counter * 2}</h1>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+    </div>
   );
 }
 
