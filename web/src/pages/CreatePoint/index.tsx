8c4665f26b5ed5d116c2f1adc446a6909673adf7
Paulo Sergio Jr <paulo.psw@gmail.com>
2020-07-01 00:01:31 -0300

    Add redirecionamento apos submit

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import api from '../../services/api';
@@ -38,6 +38,8 @@ const CreatePoint = () => {
     const [selectedCity, setSelectedCity] =  useState<string>('0');
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+
+    const history = useHistory();
 
     useEffect(() => {
         api.get('items').then(response => {
@@ -101,7 +103,9 @@ const CreatePoint = () => {
         }
         await api.post('points', data);
 
-        alert('Ponto de coleta criado!')
+        alert('Ponto de coleta criado!');
+
+        history.push('/');
     };
 
     useEffect(() => {
